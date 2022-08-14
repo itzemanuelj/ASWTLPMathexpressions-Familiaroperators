6272fd13c18032534835a4d7a8ca651266dc240a
Emanuel Joness <itzemanuelj@gmail.com>
2022-08-14 01:35:46 -0400

    intial commit for math expressins pyhon

---
+++ Math _expressions_Familiar_operators.py
@@ -0,0 +1,46 @@
+# Rewrite this so total equals 10.
+# total = "4" + 6
+total = 4 + 6
+
+# What is the value of third_number?
+# first_number = 5
+# second_number = 6
+# third_number = first_number * second_number
+30
+
+# Divide 12 by the decimal .5 and assign the result to the variable outcome
+outcome = 12 / .5
+
+# Divide the variable total by 3 and assign the result to the variable each_share.
+each_share = total /3
+
+# Subtract one number from another and assign the result to a variable. Make up the numbers and the variable name.
+cost = 10 - 5
+
+# Divide one variable by another and assign the result to a third variable. Make up the variable names.
+divide = 100 /2
+
+# Subtract a variable from a number and assign the result to a second variable. Make up the variable names and the number.
+subtract = 20 - 10
+
+# Multiply a variable by itself and assign the result to the same variable. Make up the variable name.
+time = 1
+time = time * time
+
+# Multiply three numbers and assign the result to a variable. Make up the numbers and the variable name.
+three_numbs_multiply = 3 * 3 *3
+
+# In one line of code, display the result of multiplying two variables. Make up the variable names.
+one_var = 10
+two_var = 20
+print(one_var * two_var)
+
+# Assign a number to a variable, and another number to a second variable. Multiply the two, using the variables. Assign the result to a third variable.
+var_one = 1
+var_two = 2
+var_three = var_one * var_two
+print(var_three)
+
+# In a single line of code, divide one number by another and display the result.
+one_number = 10 / 2
+print(one_number)
